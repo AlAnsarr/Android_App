8c34e14ffbadc12d5ad3b6c1374b0eedaee31515
AlAnsarr <gocer.efekan@gmail.com>
2020-03-03 18:14:00 +0100

    Accueil terminé + liste contacts en cours

--- app/src/main/java/com/example/m4104_tdapp/MainActivity.java
+++ app/src/main/java/com/example/m4104_tdapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.m4104_tdapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,16 +9,63 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    private ImageView logo;
+    private TextView titre;
+    private EditText nomEntre;
+    private Button boutton;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logo = findViewById(R.id.logoAccueil);
+        titre = findViewById(R.id.titreAccueil);
+        nomEntre = findViewById(R.id.saisieNom);
+        boutton = findViewById(R.id.boutonAccueil);
+
+        boutton.setEnabled(false);
+
+        nomEntre.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // This is where we'll check the user input
+                boutton.setEnabled(s.toString().length() != 0);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        boutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // The user just clicked
+                Intent secondActivity = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(secondActivity);
+                Toast.makeText(MainActivity.this, "Bienvenue " + nomEntre.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
